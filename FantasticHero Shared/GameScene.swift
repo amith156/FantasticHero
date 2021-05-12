9b4f613a452717182373eb3c27fa3a32b3b440b0
Amith's MacBook <amithkumarnarayan1@gmail.com>
2021-05-13 00:44:30 +0100

    hero moves from west to east and east to west within the set game area.

--- FantasticHero Shared/GameScene.swift
+++ FantasticHero Shared/GameScene.swift
@@ -9,7 +9,27 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let gameArea : CGRect
     let player = SKSpriteNode(imageNamed: "hero01")
+    
+    override init(size: CGSize) {
+        
+        let aspectRatioMax : CGFloat = 16.0/9.0
+        let playableWidth = size.height / aspectRatioMax
+
+        //gettting marging by using the center
+        let margin = (size.width - playableWidth) / 2
+        
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -19,7 +39,7 @@ class GameScene: SKScene {
         
         
         player.setScale(3)
-        player.position = CGPoint(x: self.size.width/2, y: self.size.height/5)
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height/6)
         player.zPosition = 2
         self.addChild(player)
         
@@ -27,6 +47,31 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         bulletFiring()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch : AnyObject in touches {
+            
+            let previousPointTouch = touch.previousLocation(in: self)
+            let presentPointTouch = touch.location(in: self)
+            let amountDraggedX = presentPointTouch.x - previousPointTouch.x
+            player.position.x += amountDraggedX
+            
+            //if moved too far to right, bump it back to game area
+            if(player.position.x > gameArea.maxX) {
+                player.position.x = gameArea.maxX
+            }
+            
+            //if moved too far to right, bump it back to game area
+            if (player.position.x < gameArea.minX) {
+                player.position.x = gameArea.minX
+            }
+            
+            
+        }
+        
+        
     }
 
 }
@@ -42,8 +87,7 @@ extension GameScene {
         
         let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height + 2, duration: 1.25)
         let deleteBullet = SKAction.removeFromParent()
-        let x = SKAction.sequence([moveBullet, deleteBullet])
-        bullet.run(x)
+        bullet.run(SKAction.sequence([moveBullet, deleteBullet]))
     }
     
     
