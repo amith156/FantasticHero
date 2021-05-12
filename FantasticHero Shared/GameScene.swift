db8f73ef319ae375bfb6e86304e166c74f3a8b33
Amith's MacBook <amithkumarnarayan1@gmail.com>
2021-05-12 22:17:02 +0100

    bullet firing feature

--- FantasticHero Shared/GameScene.swift
+++ FantasticHero Shared/GameScene.swift
@@ -8,7 +8,8 @@
 import SpriteKit
 
 class GameScene: SKScene {
-
+    
+    let player = SKSpriteNode(imageNamed: "hero01")
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -16,13 +17,34 @@ class GameScene: SKScene {
         background.zPosition = 0
         self.addChild(background)
         
-        let player = SKSpriteNode(imageNamed: "hero01")
+        
         player.setScale(3)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height/5)
         player.zPosition = 2
         self.addChild(player)
         
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        bulletFiring()
+    }
 
+}
 
+extension GameScene {
+    
+    func bulletFiring() {
+        
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.zPosition = 1
+        bullet.position = player.position
+        self.addChild(bullet)
+        
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height + 2, duration: 1.25)
+        let deleteBullet = SKAction.removeFromParent()
+        let x = SKAction.sequence([moveBullet, deleteBullet])
+        bullet.run(x)
+    }
+    
+    
 }
